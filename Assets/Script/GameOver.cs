ac65dfe1e6312477396f5a170938108cb54c1d95
shariful islam <sisagor98@gmail.com>
2020-03-23 06:25:20 +0600

    gameOver commit

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public Text scoretext;
+    int score = 0;
+    int count = 0;
+
+   // public GameObject ball;
+    public Vector2 respawnPoint;
+
+    private void Start()
+    {
+      //  respawnPoint = transform.position;
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag =="collide")
+        {
+            score++;
+            scoretext.text = score.ToString();
+
+        }
+
+        if(other.tag =="gameover")
+        {
+            count++;
+            ReStart();
+           // transform.position = respawnPoint;
+            /* if(count == 3)
+             {
+                 ReStart();
+             }*/
+            // Swaping();
+        }
+
+        void ReStart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+    
+}
---
+++ Assets/Script/Scripts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour
+{
+    public float speed =5f;                
+
+    private Rigidbody2D rb;
+
+    Vector2 movement;
+
+    public Vector3 respawnPoint;
+
+    void Start()
+    {  
+        rb= GetComponent<Rigidbody2D>();
+        respawnPoint = transform.position;
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+       // movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
